b1a1af486b854f05443caa15535a1caffd691813
skateriina <isip_e.a.sorokina@mpt.ru>
2025-11-26 15:56:37 +0300

    Add

--- lab1/z6-sandwiches/z6.js
+++
@@ -1,8 +0,0 @@
-// Сэндвичи
-// Нужно 2 хлеба и 1 сыр => считаем минимально возможное
-
-function countSandwiches(obj) {
-  return Math.min(Math.floor(obj.bread / 2), obj.cheese);
-}
-
-console.log(countSandwiches({ bread: 5, cheese: 6 })); // должно быть 2
